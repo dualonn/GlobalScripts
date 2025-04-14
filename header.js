e77950a323c29f31541eb93f9366cc79b6e0d91e
dualonn <williamgriffiths19@gmail.com>
2025-04-14 12:11:29 -0400

    fully working header.js

--- header.js
+++ header.js
@@ -1,30 +1,44 @@
-header = document.createElement('div')
-header.id = "header"
+header = document.createElement('div');
+header.id = "header";
 
-function setup_header(){
-    for (let link of ["Home", "Projects", "About"]){
-        let element = document.createElement('div')
-        element.className = "header_link"
-        element.innerHTML = `<p class='header_link_text'>${link}</p>`
-        element.setAttribute('links-to', `${link}`)
-        element.onclick = function(){
-            load_content.call(this)
-        }
-        header.appendChild(element)
+bodyid = document.body.id;
+
+function setup_header() {
+    for (let link of ["Home", "Projects", "About"]) {
+        let element = document.createElement('div');
+        element.className = "header_link";
+        element.innerHTML = `<p class='header_link_text'>${link}</p>`;
+        element.setAttribute('links-to', `${link}`);
+        element.onclick = function() {
+            if (typeof load_content === 'function') {
+                // If load_content is defined, call it for the main page
+                load_content.call(this);
+            } else {
+                // Otherwise, navigate to the main page with the correct query parameter
+                window.location.href = `https://dualonn.com?page=${link}`;
+            }
+        };
+        header.appendChild(element);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    setup_page()
-})
+    setup_page();
+});
 
-function setup_page(){
-    document.body.appendChild(header)
-    let icon = document.createElement('img')
-    icon.id = "dualonn-icon"
-    icon.setAttribute('links-to', "Home")
-    icon.src = "dualonn-icon.png"
-    icon.onclick = load_content
-    header.appendChild(icon)
-    setup_header()
+function setup_page() {
+    document.body.appendChild(header);
+    let icon = document.createElement('img');
+    icon.id = "dualonn-icon";
+    icon.setAttribute('links-to', "Home");
+    icon.src = "dualonn-icon.png";
+    icon.onclick = function() {
+        if (typeof load_content === 'function') {
+            load_content.call(this);
+        } else {
+            window.location.href = "https://dualonn.com";
+        }
+    };
+    header.appendChild(icon);
+    setup_header();
 }
